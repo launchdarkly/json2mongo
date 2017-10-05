93f10fde587b7cbcca074ddcd9b1f2b522640552
Zach Davis <zach@launchdarkly.com>
2017-10-05 15:52:55 -0700

    Disable eslint check to fix tests

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = function (obj) {
       case '$maxKey':
         return new mongo.MaxKey()
       case '$numberLong':
-        return new mongo.Long.fromString(val)
+        return new mongo.Long.fromString(val) // eslint-disable-line new-cap
     }
     if (typeof val === 'object') {
       obj[key] = module.exports(val)
